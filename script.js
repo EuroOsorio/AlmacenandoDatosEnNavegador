680630f730c86fa6f7f318a6ce4eefc25d9205a6
Euro <eurojoseosorio2014@gmail.com>
2022-11-21 04:23:08 -0300

    actualizacion 1

--- script.js
+++ script.js
@@ -3,16 +3,36 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento) => {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+} 
+
+const taskList = [];    // Arreglo vacio
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value;
+  const dateFormat = moment(date).format("DD/MM/YYYY");
+  console.log(dateFormat);
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
   //backticks
   const taskContent = document.createElement('div');
+  const taskObj = {
+    value,
+    dateFormat
+  };
+
+  taskList.push(taskObj); // Me adicione elemnentos al arreglo 'taskList'.
+
+// sessionStorage.setItem("tasks", JSON.stringify(taskObj));  Almasenamiento en 'sessionStorage'. 'JSON.stringify' me convierte un JavaScript value a un JSON string.
+localStorage.setItem("tasks", JSON.stringify(taskList)); // Almasenamiento en 'sessionStorage'.
 
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
@@ -20,11 +40,13 @@ const createTask = (evento) => {
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
